36d61ca032874e1498bdd8ed6d2ecb4be023102b
judah <landzbej@gmail.com>
2023-06-18 16:18:24 -0400

    node package now

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 console.log("Server started");
-var Msg = '';
+port = 8010
+var Msg = 'hi there';
+// var WebSocketServer = require('ws').Server
+//     , wss = new WebSocketServer({port: 8010});
+//     wss.on('connection', function(ws) {
+//         ws.on('message', function(message) {
+//         console.log('Received from client: %s', message);
+//         ws.send('Server received from client: ' + message);
+//     });
+//  })
 var WebSocketServer = require('ws').Server
-    , wss = new WebSocketServer({port: 8010});
+    , wss = new WebSocketServer({port});
     wss.on('connection', function(ws) {
         ws.on('message', function(message) {
         console.log('Received from client: %s', message);
         ws.send('Server received from client: ' + message);
     });
- })
+ });
+console.log("Websocket Server started on port " + port);
